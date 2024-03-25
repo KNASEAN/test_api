c0b109bf816173aa07b08c0ec274142d18466a10
KNASEAN <admin@kohnankh.com>
2024-03-25 16:58:39 +0900

    back to webapp

--- contents.js
+++ contents.js
@@ -1,4 +1,4 @@
     const env={
         liffId : "1657309677-1L2Qn6eY",
-        appurl : "https://script.googleapis.com/v1/scripts/AKfycbziCwEuufQix28ZqK2fWFKEecb_Dq6cJ48LLG7_bFYF4bpWawGboAnq51w-Y2VmnE2H:run",
+        appurl : "https://script.google.com/macros/s/AKfycbwHJD3oQKjeNPspIwLafoifrpLC7kWgkQ9UYu_OzetjwC8KWvlev-X1d4wYit57Eo8a/exec",
     }
