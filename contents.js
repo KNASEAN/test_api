4a68b5a1ce0d74907bdd5b72813c5306e23837a5
KNASEAN <admin@kohnankh.com>
2024-03-25 16:31:52 +0900

    up

--- contents.js
+++ contents.js
@@ -1,2 +1,4 @@
-    const liffId = "1657309677-1L2Qn6eY";
-    const appurl = "https://script.google.com/macros/s/AKfycbzvjVoZjcasxpaZWuZluE7SblN1OquPyFl_TZY4WtnEGyvy39CBYIT6ELCNKoAiqbqc/exec";
+    const env={
+        liffId : "1657309677-1L2Qn6eY",
+        appurl : "https://script.google.com/macros/s/AKfycbzvjVoZjcasxpaZWuZluE7SblN1OquPyFl_TZY4WtnEGyvy39CBYIT6ELCNKoAiqbqc/exec",
+    }
