d4913dd6b3a11c9c007f61f066eaedf57e62b50f
KNASEAN <admin@kohnankh.com>
2024-03-26 13:56:33 +0900

    up

--- contents.js
+++ contents.js
@@ -2,7 +2,7 @@
     //contingency constants
     const env={
         liffId : "1657309677-1L2Qn6eY",
-        appurl : "https://script.google.com/macros/s/AKfycbzvXOLV4dbmI9lUGHhe8Oj5tc_R3lOv_zuNWIUdqt_IX8GB7pHs_3cF76aym6SQ6eyD/exec",
+        appurl : "https://script.google.com/macros/s/AKfycbz0jl2yopjlOLg6H-6wqawiXFFN4_toTTwPPEyXeP9uyi7FzABSrOw3jMLyh1PHT4Un/exec",
     }
 
     //create table from array
