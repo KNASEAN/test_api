0834ef401060d81a988a8baace45da5476c83f63
KNASEAN <admin@kohnankh.com>
2024-03-27 17:07:47 +0900

    21

--- public/contents.js
+++ public/contents.js
@@ -2,7 +2,7 @@
     //contingency constants
     const env={
         liffId : "1657309677-Q0Y7E841",
-        appurl : "https://script.google.com/macros/s/AKfycbzaxiI9OX2qcl1FK6XXngsq1uyQunUe_qwFi0NIOIUEPA0Kt6olOLWwmehsLkzlJoAT/exec",
+        appurl : "https://script.google.com/macros/s/AKfycbx7AGsWqojGywEh6VMvf6_F1RKI0cjB99u3CRLtRsz56SUBpy3mdgTAwlZ55eZIu99W/exec",
         type_key:"test"
     }
 
